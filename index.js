619272d01419ab8bde1855ff0620a610a284e259
kubat1234 <kubateq@gmail.com>
2021-02-26 14:21:54 +0100

    Created bot

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+const env = process.env
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+ 
+client.login(env.token);
