646d43744248383070574a5ea328d0e26e846c46
kubat1234 <kubateq@gmail.com>
2021-03-17 13:41:40 +0100

    Fix node_modules

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const Discord = require('discord.js');
-require('dotenv').config();
+// const Discord = require('discord.js');
+// require('dotenv').config();
 
-const client = new Discord.Client();
+// const client = new Discord.Client();
 
-const env = process.env
+// const env = process.env
  
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+// client.on('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}!`);
+// });
  
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
+// client.on('message', msg => {
+//   if (msg.content === 'ping') {
+//     msg.reply('pong');
+//   }
+// });
  
-client.login(env.TOKEN);
+// client.login(env.TOKEN);
+const axios = require('axios');
+
+axios.get('https://szkopul.edu.pl/c/2015-grupa-poczatkujaca/ranking/')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  // .catch(function (error) {
+  //   // handle error
+  //   console.log(error);
+  // })
+  // .then(function () {
+  //   // always executed
+  // });
